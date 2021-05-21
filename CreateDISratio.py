e039a315c0191c1861c41021c9ab55487de1c885
Wood <wood5@canisius.edu>
2021-05-21 08:54:02 -0400

    Latest version of CreateDISratio.py

--- CreateDISratio.py
+++ CreateDISratio.py
@@ -6,8 +6,10 @@ xhi = 1.0
 xbins = 40
 xstep = (xhi-xlo)/xbins
 numbers = [round(xlo + (x + 1/2)* xstep,5) for x in range(0, xbins)]
-#repeat = 45000
-repeat = 92500
+# Carbon (89303)
+# Iron (94260)
+# Lead (45515)
+repeat = 45515
 #repeat = 100000
 for i in range(repeat):
 	print(*numbers, sep = "\n")
